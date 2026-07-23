0aaf753928fc256d4926307ba2d628f483bed8a7
jchiran010 <jchiran010@github.com>
2026-07-23 09:22:01 +0530

    Fix: remove unused Bcrypt import and heavy requirements (pandas, numpy, gunicorn) to ensure successful Vercel serverless runs

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -20,7 +19,6 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(db_dir, 'complaints.db')
 
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
